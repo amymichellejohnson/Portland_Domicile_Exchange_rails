20820d3c66b2dd8b49c2b30b18e7321c19754990
Amy Johnson and Lauren Marcus <lauren.e.marcus@gmail.com>
2015-03-25 10:49:23 -0700

    Validate presence for listings attributes

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,7 @@
 class Listing < ActiveRecord::Base
   belongs_to :user
+  validates :price, :presence => true
+  validates :title, :presence => true
+  validates :details, :presence => true
+  validates :location, :presence => true
 end
--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -2,5 +2,9 @@ require 'rails_helper'
 
 describe Listing do
   it {should belong_to :user}
+  it { should validate_presence_of :price}
+  it { should validate_presence_of :title}
+  it { should validate_presence_of :details}
+  it { should validate_presence_of :location}
 
 end
